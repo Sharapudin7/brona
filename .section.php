23073969579e54770a2bc2db469c65fc5606e73e
ftp <shiraeg6_11@stingray.beget.ru>
2022-04-05 11:39:54 +0300

    first commit

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Брона - Магазин дверей',
+	'description' => 'Брона - Магазин дверей',
+	'keywords' => 'двери, фурнитура ',
+	'robots' => 'index, follow'
+);
+?>
